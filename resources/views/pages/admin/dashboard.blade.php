da0816c2430974db292ea171a7e52d066788b9fa
Dustin <rdhammack88@hotmail.com>
2018-07-06 00:14:33 -0500

    Created about & dashboard view. Added bootstrap navbar to navbar view. Moved @includes/navbar outside of #app div.

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app')
+<script type="text/javascript">
+    document.title = 'Admin Home Page!';
+</script>
 
 @section('content')
 
 <h1>Admin Home Page!</h1>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="alert alert-success">
+            You are logged in!
+        </div>
+    </div>
+</div>
 
 {{-- @if (count($questions) > 0)
     @foreach ($questions as $question)
