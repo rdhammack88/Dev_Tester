892259a78269f73e956730a837da67255b592b63
Dustin <rdhammack88@hotmail.com>
2018-07-07 01:06:30 -0500

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,38 @@
 */
 
 Auth::routes();
+Route::resource('/quiz-choice', 'QuizChoicesController'); //PagesController@quizChoice
+Route::resource('/question', 'QuestionsController');
+
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
+Route::get('/ajax/{count}/{route}', 'AjaxController@index');
+Route::get('/quizFinal', 'QuestionsController@quizComplete');
+Route::get('/admin/dashboard', 'DashboardController@index');
+Route::get('/admin/edit-question/{id}', 'DashboardController@editQuestion');
+Route::get('/admin/add-question', 'QuestionsController@create');
 
+Route::post('/question/{category}/{num}', 'QuestionsController@showQuestion');
+
+
+// Auth::routes();
 // Route::get('/admin-login', 'PagesController@adminLogin');
 // Route::get('/admin-register', 'PagesController@adminRegister');
-Route::get('/admin/dashboard', 'DashboardController@index');
 
+// Route::get('/admin/add-question', 'DashboardController@addQuestion');
 
-Route::resource('/quiz-choice', 'QuizChoicesController'); //PagesController@quizChoice
-
-Route::get('/ajax/{count}/{route}', 'AjaxController@index');
 // Route::get('/ajax/admin/login', 'AjaxController@loginSwitch');
 // Route::get('/ajax/admin/register', 'AjaxController@registerSwitch');
 
 // Auth::routes();
-Route::get('/question', 'QuestionsController@showQuestion');
-Route::post('/question', 'QuestionsController@showQuestion');
-Route::post('/question/{category}/{num}', 'QuestionsController@showQuestion');
-
-Route::get('/quizFinal', 'QuestionsController@quizComplete');
+// Route::get('/question', 'QuestionsController@showQuestion');
+// Route::post('/question', 'QuestionsController@showQuestion');
 
 // Route::post('/question/{category}', 'QuizChoicesController@showQuestion'); //'PagesController@question');
 
 // /questions
 
 // Route::get('/question-count', 'QuestionsController@index');
-
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
