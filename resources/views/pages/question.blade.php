cbab5d64f5c4487f87e13b9d4c5b504c653ac68f
Dustin <rdhammack88@hotmail.com>
2018-07-30 01:14:21 -0500

    Removed tests in production page of question.blade.php

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -1,34 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- {{ $question_count }} --}}
-    {{-- {{ $question }}
-    <br><br>
-    {{ $question['id'] }}
-    <br>
-    {{ $question['question_category'] }}
-    <br>
-    {{ $question['question'] }}
-    <br>
-    {{ $question['added_by'] }}
-    <br>
-
-    <br>
-    {{  $question_id }}
-    <br>
-    {{  $answers }}
-    <br>
-
-    @foreach ($answers as $key => $value)
-        <p>{{ $key . ' -> ' . $value['answers'] }}</p>
-    @endforeach --}}
-
-    {{-- <br>
-    {{ $question_id }}
-    {{ $question['id'] }}
-    <br> --}}
-
-
     <main class="questionPage">
 		<div class="container">
 			<form method="post" action="/question/{{ $question['question_category'] }}/{{ $question_number + 1 }}">
@@ -50,7 +22,7 @@
                                 <input type="hidden" name="is_correct" value="{{ $answer['is_correct'] }}">
                                 <input type="radio" name="choice" value="{{ $answer['id'] }}" id="{{ 'answer' . $answer['id'] }}">
                                 &nbsp;&nbsp;
-                                <label for="{{ 'answer' . $answer['id'] }}">{{ $answer['answers'] }} -- {{ $answer['id'] }}</label>
+                                <label for="{{ 'answer' . $answer['id'] }}">{{ $answer['answers'] }}</label>
                             </li>
                         @endforeach
 
@@ -72,47 +44,5 @@
 				</div>
 			</form>
 		</div>
-
-
-
-            {{ $recent_choice }}
-            {{-- {{ session('user.answers') }} --}}
-
-            {{ print_r(Session::all()) }}
-            {{-- @foreach ($previous_questions as $question)
-                <p>{{ $question }}</p>
-            @endforeach --}}
-
-            {{-- @foreach ($user['answers'] as $user_answer)
-                <p>{{ $user_answer }}</p>
-            @endforeach --}}
-            {{-- {{ $previous_questions }} --}}
-<br><br><br><br><br>
-            @foreach (session('question.previous_questions') as $question)
-                <p>{{ $question }}</p>
-            @endforeach
-<br><br><br><br><br>
-            @foreach (session('question.correct_answers') as $correct_answers)
-                {{-- @foreach ($key as $correct_answers => $answer)
-                <p>{{ $answer }}</p> --}}
-                    <p>{{ $correct_answers }}</p>
-                {{-- @endforeach --}}
-            @endforeach
-<br><br><br><br><br>
-    @if(session('question.user_answers'))
-            @foreach (session('question.user_answers') as $user_answers)
-                <p>{{ $user_answers }}</p>
-            @endforeach
-        @endif
-
-        <br><br><br><br><br><br>
-        <p>Recenet Questions</p>
-        @foreach ($recent_questions as $recent_question)
-            {{ $recent_question }}
-        @endforeach
-
-            {{-- {{ session('previous_questions') }} --}}
 	</main>
-
-
 @endsection
