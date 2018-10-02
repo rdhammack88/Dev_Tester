d9c0b2838f7cc647016d707fc222a1c43c88af70
Dustin <rdhammack88@hotmail.com>
2018-10-02 02:09:49 -0500

    Added bootstrap active class based on route

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -23,43 +23,44 @@ if(Session::get('category') !== null) {
 
 ?>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+				 aria-controls="navbar">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
-			</button>
-			{{-- <a class="navbar-brand" href="#">Project name</a> --}}
-			<a href="/" class="navbar-brand">{{ $title }}</a>
-		</div>
-		<div id="navbar" class="collapse navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<!-- Authentication Links -->
-				@if (Auth::guest())
+			</button> {{-- <a class="navbar-brand" href="#">Project name</a> --}}
+				<a href="/" class="navbar-brand">{{ $title }}</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="{{Request::is('/') ? 'active' : ''}}"><a href="/">Home</a></li>
+					<li class="{{Request::is('/about') ? 'active' : ''}}"><a href="/about">About</a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<!-- Authentication Links -->
+					@if (Auth::guest())
 					<li><a href="{{ route('login') }}">Login</a></li>
 					<li><a href="{{ route('register') }}">Register</a></li>
-				@else
-					<li><a href="/admin/dashboard">Dashboard</a></li>
+					@else
+					<li class="{{Request::is('/dashboard') ? 'active' : ''}}"><a href="/admin/dashboard">Dashboard</a></li>
 					<li>
-                        <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
+						<a href="{{ route('logout') }}" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
                             Logout
                         </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-				@endif
-			</ul>
-		</div><!--/.nav-collapse -->
-	</div>
-</nav>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							{{ csrf_field() }}
+						</form>
+					</li>
+					@endif
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</nav>
